8bd70b12aac5ade04e45f6db96b15a0cfd52fc10
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-12-14 13:42:15 +1100

    Added override mode

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@ require 'yaml'
 node[:deploy].each do |application, deploy|
   (deploy['rails_config'] || []).each do |filename, config|
     file File.join(deploy['deploy_to'], 'shared', 'config', filename + '.yml') do
-      content YAML.dump(deploy['rails_env'] => config).gsub(/!ruby\/.+$/, '')
+      content config.is_a? String ? config : YAML.dump(deploy['rails_env'] => config).gsub(/!ruby\/.+$/, '')
       user deploy[:user]
       group deploy[:group]
       mode 0770
